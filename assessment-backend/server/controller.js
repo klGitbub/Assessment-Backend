ded697a88b2bd3a91ca774b3743487dc7f5984db
klGitbub <klaghaeian@gmail.com>
2023-01-13 14:51:14 -0500

    Initial commit

---
+++ assessment-backend/server/controller.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+
+    getFortune: (req, res) => {
+        const fortunes = ['Failure is the path of lease persistence.', 'Place special emphasis on old friendship.', 'Today is the conserve yourself, as things just won’t budge.', 'You have the power to write your own fortune.', 'Your energy returns and you get things done.'];
+
+        // choose random fortune
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+
+        res.status(200).send(randomFortune);
+    },
+
+}
